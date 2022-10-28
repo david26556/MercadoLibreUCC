3c42031bafd2869bd1662bce5725ff30028f22a9
david26556 <115234246+david26556@users.noreply.github.com>
2022-10-27 21:41:38 -0500

    Add files via upload

---
+++ LOGIN_FORM/js/script.js
@@ -0,0 +1,11 @@
+
+//DECLARACION DE VARIABLES
+document.getElementById("btn_logIn").addEventListener("click",IniciarSesion);
+
+
+
+
+function IniciarSesion(){
+    $("#result").text("");
+    $("#result")="BIENVENIDO!";
+}
